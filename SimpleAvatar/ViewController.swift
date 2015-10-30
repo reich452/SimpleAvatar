4cbd641168bf53f57f1c968ba7c0ed6158c519a5
Ben Norris <ben@bsn.io>
2015-10-30 07:58:52 -0600

    Initial Commit

---
+++ SimpleAvatar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAvatar
+//
+//  Created by Ben Norris on 10/30/15.
+//  Copyright © 2015 Dev Mountain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
