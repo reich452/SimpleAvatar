19f456a2a42eed94370e0aa192fcd6a888a7867d
reich452 <reich452@regis.edu>
2017-03-27 19:41:44 -0600

    Added notes

--- SimpleAvatar/SimpleAvatarView.swift
+++ SimpleAvatar/SimpleAvatarView.swift
@@ -10,46 +10,46 @@ import UIKit
 
 import UIKit
 
-@IBDesignable public class SimpleAvatarView: UIView {
+@IBDesignable open class SimpleAvatarView: UIView {
     
     // MARK: - Properties
     
-    private let backgroundView = UIView()
-    private let initialsLabel = UILabel()
-    private let imageView = UIImageView()
-    private let margin: CGFloat = 2.0
+    fileprivate let backgroundView = UIView()
+    fileprivate let initialsLabel = UILabel()
+    fileprivate let imageView = UIImageView()
+    fileprivate let margin: CGFloat = 2.0
     
-    @IBInspectable public var borderColor: UIColor = UIColor(red: 50 / 255.0, green: 137 / 255.0, blue: 68 / 255.0, alpha: 1.0) {
+    @IBInspectable open var borderColor: UIColor = UIColor(red: 50 / 255.0, green: 137 / 255.0, blue: 68 / 255.0, alpha: 1.0) {
         didSet {
             updateColors()
         }
     }
     
-    @IBInspectable public var innerBackgroundColor: UIColor = UIColor(red: 140 / 255.0, green: 189 / 255.0, blue: 74 / 255.0, alpha: 1.0) {
+    @IBInspectable open var innerBackgroundColor: UIColor = UIColor(red: 140 / 255.0, green: 189 / 255.0, blue: 74 / 255.0, alpha: 1.0) {
         didSet {
             updateColors()
         }
     }
     
-    @IBInspectable public var borderWidth: CGFloat = 8.0 {
+    @IBInspectable open var borderWidth: CGFloat = 8.0 {
         didSet {
             updateBorder()
         }
     }
     
-    @IBInspectable public var firstName: String? {
+    @IBInspectable open var firstName: String? {
         didSet {
             updateInitials()
         }
     }
 
-    @IBInspectable public var lastName: String? {
+    @IBInspectable open var lastName: String? {
         didSet {
             updateInitials()
         }
     }
     
-    @IBInspectable public var image: UIImage? {
+    @IBInspectable open var image: UIImage? {
         didSet {
             imageView.image = image
         }
@@ -71,7 +71,7 @@ import UIKit
     
     // MARK: - Public API
     
-    public func reset() {
+    open func reset() {
         imageView.image = nil
         initialsLabel.text = nil
     }
@@ -79,13 +79,13 @@ import UIKit
     
     // MARK: - Customized view methods
     
-    override public func layoutSubviews() {
+    override open func layoutSubviews() {
         super.layoutSubviews()
         
         let minSideSize = min(frame.size.width, frame.size.height)
         layer.cornerRadius = minSideSize / 2.0
         
-        initialsLabel.font = UIFont.systemFontOfSize(minSideSize / 2.5, weight: UIFontWeightLight)
+        initialsLabel.font = UIFont.systemFont(ofSize: minSideSize / 2.5, weight: UIFontWeightLight)
     }
     
 }
@@ -104,34 +104,34 @@ private extension SimpleAvatarView {
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(backgroundView)
 
-        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1.0, constant: 0.0))
-        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1.0, constant: 0.0))
-        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1.0, constant: 0.0))
-        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: backgroundView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0))
 
         initialsLabel.translatesAutoresizingMaskIntoConstraints = false
-        initialsLabel.textAlignment = .Center
+        initialsLabel.textAlignment = .center
         addSubview(initialsLabel)
         
-        initialsLabel.centerYAnchor.constraintEqualToAnchor(centerYAnchor).active = true
-        initialsLabel.leadingAnchor.constraintEqualToAnchor(leadingAnchor, constant: margin).active = true
-        trailingAnchor.constraintEqualToAnchor(initialsLabel.trailingAnchor, constant: margin).active = true
+        initialsLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        initialsLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: margin).isActive = true
+        trailingAnchor.constraint(equalTo: initialsLabel.trailingAnchor, constant: margin).isActive = true
         
         // Add image as an overlay to hide initials once it's been added
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .ScaleAspectFill
+        imageView.contentMode = .scaleAspectFill
         addSubview(imageView)
         
         let views = ["backgroundView": backgroundView, "initialsLabel": initialsLabel, "imageView": imageView]
         let metrics = ["margin": margin]
         
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[imageView]|", options: [], metrics: metrics, views: views))
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[imageView]|", options: [], metrics: metrics, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[imageView]|", options: [], metrics: metrics, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[imageView]|", options: [], metrics: metrics, views: views))
     }
     
     func updateColors() {
         backgroundView.backgroundColor = innerBackgroundColor
-        layer.borderColor = borderColor.CGColor
+        layer.borderColor = borderColor.cgColor
         initialsLabel.textColor = borderColor
     }
     
@@ -145,16 +145,16 @@ private extension SimpleAvatarView {
     
     func initials() -> String {
         var initialsString = String()
-        if let firstName = firstName where firstName.characters.count > 0 {
-            initialsString += firstName.substringToIndex(firstName.startIndex.successor())
+        if let firstName = firstName, firstName.characters.count > 0 {
+            initialsString += firstName.substring(to: firstName.characters.index(after: firstName.startIndex))
         }
-        if let lastName = lastName where lastName.characters.count > 0 {
-            initialsString += lastName.substringToIndex(lastName.startIndex.successor())
+        if let lastName = lastName, lastName.characters.count > 0 {
+            initialsString += lastName.substring(to: lastName.characters.index(after: lastName.startIndex))
         }
         if initialsString.characters.count == 0 {
             initialsString = "?"
         }
-        return initialsString.uppercaseString
+        return initialsString.uppercased()
     }
     
 }
--- SimpleAvatar/ViewController.swift
+++ SimpleAvatar/ViewController.swift
@@ -12,13 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    //-------------------- Notes  ----------------
+    // @IBOutlet - gives you a connection to that conroll or to that view and your code
+    // You are using an outlet when you want to change something on the code. 
+    // @IBAction -  lets you respond to user ouput! 
+    
+    // @IBInspectable properties provide new access to an old feature: user-defined runtime attributes. Currently accessible from the identity inspector, these attributes have been available since before Interface Builder was integrated into Xcode. They provide a powerful mechanism for configuring any key-value coded property of an instance in a NIB, XIB, or storyboard:
+    
+    // Designing Intuitive User Experiences
+    
+    // didSet - when it sets something it is going to happen 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
